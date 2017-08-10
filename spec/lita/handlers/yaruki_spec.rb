6273ef62fd9351b4bb4b27f95c08e5c1b1f53bb2
gouf <innocent.zero@gmail.com>
2017-08-11 01:16:34 +0900

    Add some rspec test for handler

--- spec/lita/handlers/yaruki_spec.rb
+++ spec/lita/handlers/yaruki_spec.rb
@@ -1,4 +1,16 @@
 require "spec_helper"
 
+# In development environment, adjust redis host/port setting if you using docker or whatever
+# Lita.configure do |config|
+#   config.redis[:host] = 'localhost'
+#   config.redis[:port] = '6379'
+# end
+
 describe Lita::Handlers::Yaruki, lita_handler: true do
+  it { is_expected.to route('yaruki') }
+  it { is_expected.to route('yaruki').to(:yaruki) }
+  it 'retrieve message of "yaruki"' do
+    send_message('yaruki')
+    expect(replies.last).to match(/[0-9]{,3}.*/)
+  end
 end
